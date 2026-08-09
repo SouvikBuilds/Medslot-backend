022fc6617eef436626dca47bdfbf2831ecc7f5c7
Souvik Chatterjee <bravoth0729v@gmail.com>
2026-08-09 16:53:13 +0530

    resolved public/temp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import config from "./config/envConfig.js";
 import { connectDB } from "./db/config.js";
 import app from "./app.js";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+fs.mkdirSync(path.join(__dirname, "../public/temp"), { recursive: true });
 
 await connectDB()
   .then(() => {
