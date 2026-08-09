0832ba91994b5ca2e40e53cc89ecee689817095b
Souvik Chatterjee <bravoth0729v@gmail.com>
2026-08-09 16:50:33 +0530

    multer middleware changes

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,8 +1,12 @@
 import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, path.join(__dirname, "../../public/temp"));
   },
 
   filename(req, file, cb) {
